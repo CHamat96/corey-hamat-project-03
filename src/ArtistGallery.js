5321f5be5f5adfba34f1dcaeef0b8d03f6f151d5
Corey <coreyhamat@gmail.com>
2021-10-05 15:02:05 -0400

    removed redundant label in artistContainer 'form'

--- src/ArtistGallery.js
+++ src/ArtistGallery.js
@@ -20,7 +20,6 @@ function DisplayArtists(props){
       <form action="" 
       onSubmit={handleArtistChoice}>
         <img src={props.photo} alt={props.name} />
-        <label htmlFor="artistChoice" sr-only>Select a song by {props.name}</label>
         <button type='submit' id={props.id} onClick={artistSelect} value={artistChoice} name="artistChoice"> {props.name}</button>
       </form>      
     </div>
